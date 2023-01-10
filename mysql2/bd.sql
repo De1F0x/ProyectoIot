b6243a738aa2532140791f18fd231ffb6f709d32
Diego Miquelez <diego26ftw@gmail.com>
2023-01-10 02:04:18 +0100

    sadsad

---
+++ mysql2/bd.sql
@@ -0,0 +1,5 @@
+CREATE TABLE temperatura(time TIMESTAMP, temp FLOAT);
+CREATE TABLE luminosidad(time TIMESTAMP, lum FLOAT);
+CREATE TABLE humedad(time TIMESTAMP, hum FLOAT);
+CREATE TABLE distancia(time TIMESTAMP, dist FLOAT);
+CREATE TABLE movimiento(time TIMESTAMP, move BOOLEAN);
